df7f567e215d2fa66e7e49beb583db4a0714e433
FJada <124617739+FJada@users.noreply.github.com>
2024-02-28 04:20:11 -0500

    added basic player movement

---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input for horizontal and vertical movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement direction
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
+
+        // Move the player
+        transform.Translate(movement);
+
+    }
+}
+
